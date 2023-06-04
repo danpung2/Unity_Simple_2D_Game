67ca979098d139b117b281c2bd4472024f582606
danpung2 <danpung_2@naver.com>
2023-06-04 18:24:21 +0900

    CREATE: trigger between pin to target

--- Pin.cs
+++ Pin.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pin : MonoBehaviour
 {
+    private const String target = "Target";
+    private bool isPinned = false;
     [SerializeField] private float moveSpeed = 10f;
     
     // Start is called before the first frame update
@@ -15,6 +19,18 @@ public class Pin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up * (moveSpeed * Time.deltaTime);
+        if (!isPinned)
+        {
+            transform.position += Vector3.up * (moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        isPinned = true;
+        if (other.gameObject.CompareTag(target))
+        {
+            transform.SetParent(other.gameObject.transform);
+        }
     }
 }
